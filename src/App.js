a5ddf8d73259a9aaf830eb09a5a502f30088985a
zbys1av <zbyslav13@gmail.com>
2023-12-27 20:42:56 +0200

    snow effect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import logo from './logo.svg';
 import './App.scss';
+import { useCallback, useState } from 'react';
+// import Particles from 'react-tsparticles'
+// import { loadFull } from 'tsparticles';
+import Snowfall from 'react-snowfall';
 
 import Header from './Components/Header';
 import Main from './Components/Main';
@@ -11,8 +15,23 @@ import Footer from './Components/Footer';
 import Skills from './Components/Skills';
 
 function App() {
+
+  let date = new Date();
+  const months = [ "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12" ];
+  let month = false;
+  if (months[date.getMonth()] === "01" || months[date.getMonth()] === "02" || months[date.getMonth()] === "12" ){
+    month = true;
+  } else {
+    month = false;
+  }
+
+  const snowStyle = {
+    height: "220%",
+  }
+
   return (
     <div className="app">
+      {month ? <Snowfall snowflakeCount={50} style={snowStyle}/> : ""}
       <Header/>
       <Main/>
       <Study/>
